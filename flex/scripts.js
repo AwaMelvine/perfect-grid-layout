37bfd515d5444833889e2b869657d935c8207a6d
AwaMelvine <melvineawa@gmail.com>
2020-03-28 14:18:21 +0100

    Refine both layout designs to be uniform

--- flex/scripts.js
+++ flex/scripts.js
@@ -1,4 +1,4 @@
-const data = ['A', 'B', 'C', 'D', 'E', 'F', 'D'];
+const data = ['A', 'B', 'C', 'D', 'E', 'F', 'D', 'H'];
 const wrapper = document.querySelector('.wrapper');
 
 data.forEach(letter => {
